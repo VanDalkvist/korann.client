e0bdaceb01ccd782dda4cfa4735c0ef03d60bea8
Ivan Afanasov <i_van@list.ru>
2014-08-27 15:06:35 +0400

    added bower to web project; added bundle configuration; removed references;

--- Korann/App_Start/BundlesConfig.cs
+++ Korann/App_Start/BundlesConfig.cs
@@ -13,28 +13,24 @@ namespace Korann.App_Start
 
         private static void RegisterScriptBundles(BundleCollection bundles)
         {
-            // register scripts from references
+            // register vendors
 
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/references/angular/angular.js",
-                "~/Scripts/references/angular/angular-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/angular-extensions").IncludeDirectory(
-                "~/Scripts/references/angular.extensions/", "*.js", searchSubdirectories: true));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").IncludeDirectory(
-                "~/Scripts/references/jquery", "*.js"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/optimization").Include(
-                "~/Scripts/references/optimization/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").IncludeDirectory(
-                "~/Scripts/references/bootstrap/", "*.js"));
+            bundles.Add(
+                new ScriptBundle("~/bundles/angular")
+                    .Include(
+                        "~/vendors/angular/angular.js",
+                        "~/vendors/angular-resource/angular-resource.js",
+                        "~/vendors/angular-bootstrap/ui-bootstrap-tpls.js",
+                        "~/vendors/angular-ui-router/release/angular-ui-router.js",
+                        "~/vendors/lodash/dist/lodash.js",
+                        "~/vendors/checklist-model/checklist-model.js",
+                        "~/vendors/ng-file-upload/angular-file-upload.js"));
 
             // register application scripts
 
-            bundles.Add(new ScriptBundle("~/bundles/app").IncludeDirectory(
-                "~/Scripts/app/", "*.js", searchSubdirectories: true));
+            bundles.Add(new ScriptBundle("~/bundles/app")
+                .Include("~/app/main.js")
+                .IncludeDirectory("~/app/", "*.js", searchSubdirectories: true));
         }
 
         private static void RegisterStyleBundles(BundleCollection bundles)
@@ -42,8 +38,9 @@ namespace Korann.App_Start
             bundles.Add(new StyleBundle("~/bundles/styles").IncludeDirectory(
                 "~/Content/styles/", "*.css", searchSubdirectories: true));
 
-            bundles.Add(new StyleBundle("~/bundles/bootstrap-styles").IncludeDirectory(
-                "~/Content/bootstrap", "*.css", searchSubdirectories: true));
+            // todo: style from vendors
+//            bundles.Add(new StyleBundle("~/bundles/bootstrap-styles").IncludeDirectory(
+//                "~/Content/bootstrap", "*.css", searchSubdirectories: true));
         }
     }
 }
